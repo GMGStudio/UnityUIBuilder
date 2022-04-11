73c4effea97e7b6349602fd9f0f63484047d65ea
BjoernTheDev <mail@bjoern-hempel.de>
2022-04-11 21:31:12 +0200

    Initial commit

---
+++ UIBuilder/Assets/UIBuilder/UiInputManager.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class UiInputManager : MonoBehaviour
+{
+    Button StartButton;
+    Button HighscoreButton;
+    Button LeaveButton;
+
+    private void Awake()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        StartButton = root.Q<Button>("StartGame");
+        HighscoreButton = root.Q<Button>("Highscore");
+        LeaveButton = root.Q<Button>("LeaveGame");
+
+        StartButton.clicked += StartGame;
+        HighscoreButton.clicked += ShowHighscore;
+        LeaveButton.clicked += LeaveGame;
+
+    }
+
+    private void StartGame()
+    {
+        Debug.Log("Starting Game");
+        SceneManager.LoadScene("GameScene");
+    }
+
+    private void ShowHighscore()
+    {
+        Debug.Log("Showing Highscore");
+    }
+
+    private void LeaveGame()
+    {
+        Debug.Log("Leaving Game");
+        Application.Quit();
+    }
+
+    private void OnDisable()
+    {
+
+        StartButton.clicked -= StartGame;
+        HighscoreButton.clicked -= ShowHighscore;
+        LeaveButton.clicked -= LeaveGame;
+    }
+}
